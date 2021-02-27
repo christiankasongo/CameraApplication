c810b64540220241b786e5c43b0f74af4079d5a2
Christian Kasongo <christiankasongo03@gmail.com>
2021-02-27 21:03:19 +0000

    front or back camera

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ var OperatingSystem = {
 //***************************** OPEN THE CAMERA BY ASKING USER PERMISSION(APPLE DEVICE) AND APPLY VIDEO STREAM SETTINGS ***********************************
 
 const constraints = {
-  video: true,
+  video: { facingMode: (front? "user" : "environment")},
   width: { min: 1440, ideal: 1280, max: 3984 },
   height: { min: 1080, ideal: 720, max: 2988 },
   aspectRatio: 4/3,
